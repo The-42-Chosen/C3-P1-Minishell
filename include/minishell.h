cb33125618abffd316fb39d856a8ac7b32ad1903
erpascua <erpascua@student.42.fr>
2025-08-13 19:29:57 +0200

    (feat) Builtin: enums ans initialisation

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: erpascua <erpascua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 14:31:03 by erpascua          #+#    #+#             */
-/*   Updated: 2025/08/12 20:10:21 by erpascua         ###   ########.fr       */
+/*   Updated: 2025/08/13 19:14:59 by erpascua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,27 @@ typedef struct s_env
 // Pour traiter les $? il nous faudra mettre a jour constamment nos retours
 // de fonction d'ou le last_status;
 
+// # define BI_ECHO "echo"
+// # define BI_CD "cd"
+// # define BI_PWD "pwd"
+// # define BI_EXPORT "export"
+// # define BI_UNSET "unset"
+// # define BI_ENV "env"
+// # define BI_EXIT "exit"
+
+typedef enum e_builtin
+{
+	BI_ECHO,
+	BI_CD,
+	BI_PWD,
+	BI_EXPORT,
+	BI_UNSET,
+	BI_ENV,
+	BI_EXIT,
+	NB_BUILTINS
+}	t_builtin;
+
+
 typedef struct s_msh
 {
 	t_env			*env;
@@ -49,10 +70,23 @@ typedef struct s_msh
 	char			*history;
 	bool			is_heredoc;
 	bool			is_builtin;
+	bool			is_tty;
+	char			*builtin_names[NB_BUILTINS];
+	int				(*builtin_funcs[NB_BUILTINS])(void);
 }					t_msh;
 
 int					launch_program(t_msh *msh);
 void				print_banner(void);
 void				struct_init(t_msh *msh);
+
+// BUILT-IN
+bool				is_builtin(t_msh *msh, char *s);
+int					bi_exit(void);
+int					bi_echo(void);
+int					bi_cd(void);
+int					bi_pwd(void);
+int					bi_export(void);
+int					bi_unset(void);
+int					bi_env(void);
 
 #endif
