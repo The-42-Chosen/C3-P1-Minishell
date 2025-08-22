5dc23ec6876ae90f6867d45f951c76f8c0aa75db
erpascua <erpascua@student.42.fr>
2025-08-22 11:18:49 +0200

    (fix) Trying to resolve conflicts

--- src/main.c
+++ src/main.c
@@ -6,11 +6,9 @@
 /*   By: erpascua <erpascua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 10:57:36 by erpascua          #+#    #+#             */
-<<<<<<< HEAD
 /*   Updated: 2025/08/22 11:06:36 by erpascua         ###   ########.fr       */
-=======
+
 /*   Updated: 2025/08/11 18:40:38 by erpascua         ###   ########.fr       */
->>>>>>> df10aac ((feat) Minishell program launch setup on progress)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +19,7 @@ int	main(int ac, char **av, char **env)
 	(void)ac;
 	(void)av;
 	(void)env;
-<<<<<<< HEAD
+
 	print_banner();
-=======
->>>>>>> df10aac ((feat) Minishell program launch setup on progress)
 	launch_program();
 }
