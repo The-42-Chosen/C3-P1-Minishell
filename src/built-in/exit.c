0b3c833148192ecbb5e4627373b1c5d34d4d8681
RicoWD <pascual.machio.eric@gmail.com>
2025-09-16 09:33:34 +0200

    (feat) Builtin - Exit: Implemented and at norm

--- src/built-in/exit.c
+++ src/built-in/exit.c
@@ -3,18 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: erpascua <erpascua@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ep <ep@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 16:34:10 by erpascua          #+#    #+#             */
-/*   Updated: 2025/08/13 17:06:43 by erpascua         ###   ########.fr       */
+/*   Updated: 2025/09/16 09:30:45 by ep               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static bool	is_valid_code_nb(char *str)
+{
+	int	j;
+
+	j = 0;
+	if (str[j] == '+' || str[j] == '-')
+		j++;
+	while (str[j])
+	{
+		if (!ft_isdigit(str[j]))
+			return (false);
+		j++;
+	}
+	return (true);
+}
+
 int	bi_exit(char **argv)
 {
-	(void)argv; // Unused parameter
+	int	argc;
+
+	argc = 0;
 	ft_putendl_fd("exit", 1);
-	return (0);
+	while (argv[argc])
+		argc++;
+	if (argc >= 2)
+	{
+		if (!is_valid_code_nb(argv[1]))
+		{
+			ft_fprintf(2, "minishell: exit: %s: numeric argument required\n",
+				argv[1]);
+			g_exit_code = 255;
+			exit(g_exit_code);
+		}
+		g_exit_code = ft_atoi(argv[1]) % 256;
+	}
+	if (argc > 2)
+	{
+		ft_fprintf(2, "minishell: exit: too many arguments\n");
+		g_exit_code = 1;
+		exit(1);
+	}
+	exit(g_exit_code);
 }
