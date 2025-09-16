11ef1760c0b2ccd8174e16fc9e4facbc88f7c335
RicoWD <pascual.machio.eric@gmail.com>
2025-09-16 07:58:38 +0200

    (fix) Builtin - is_building (detector) changed to be adapted to 'char **'

--- src/built-in/is_builtin.c
+++ src/built-in/is_builtin.c
@@ -14,19 +14,25 @@
 
 bool	is_builtin(t_msh *msh)
 {
-	int	i;
+	int		i;
+	char	**argv;
 
 	i = 0;
+	argv = split_spaces(msh->entry);
+	if (!argv || !argv[0])
+		return (0);
 	while (i < NB_BUILTINS)
 	{
-		if (!ft_strncmp(msh->builtin_names[i], msh->entry, ft_strlen(msh->entry)
-				+ 1))
+		if (!ft_strncmp(msh->builtin_names[i], argv[0], ft_strlen(msh->builtin_names[i]) + 1))
 		{
-			msh->builtin_funcs[i]();
+			msh->builtin_funcs[i](argv);
+			free_tab(argv);
 			msh->is_builtin = 1;
 			return (1);
 		}
 		i++;
 	}
+	free_tab(argv);
+	msh->is_builtin = 0;
 	return (0);
 }
