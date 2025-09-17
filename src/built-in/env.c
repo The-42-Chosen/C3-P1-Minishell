4f18a759d4c718ad76486dc3236c792ee2f655ef
RicoWD <pascual.machio.eric@gmail.com>
2025-09-17 03:52:00 +0200

    (feat): Env - Implemented & env added to msh.env

--- src/built-in/env.c
+++ src/built-in/env.c
@@ -6,15 +6,98 @@
 /*   By: ep <ep@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 19:06:05 by erpascua          #+#    #+#             */
-/*   Updated: 2025/09/16 07:53:00 by ep               ###   ########.fr       */
+/*   Updated: 2025/09/17 03:44:30 by ep               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	bi_env(char **argv)
+static char	*safe_strdup_substr(const char *src, size_t len)
 {
-	(void)argv;
-	ft_putendl_fd("env", 1);
+	char	*dst;
+
+	dst = malloc(len + 1);
+	if (!dst)
+		return (NULL);
+	ft_memcpy(dst, src, len);
+	dst[len] = '\0';
+	return (dst);
+}
+
+static t_env	*create_env_node(char *env_line)
+{
+	char	*equal_pos;
+	t_env	*new_node;
+
+	if (!env_line)
+		return (NULL);
+	equal_pos = ft_strchr(env_line, '=');
+	if (!equal_pos)
+		return (NULL);
+	new_node = malloc(sizeof(t_env));
+	if (!new_node)
+		return (NULL);
+	new_node->key = safe_strdup_substr(env_line, equal_pos - env_line);
+	new_node->value = ft_strdup(equal_pos + 1);
+	new_node->next = NULL;
+	if (!new_node->key || !new_node->value)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (NULL);
+	}
+	return (new_node);
+}
+
+static void	add_env_node(t_env **head, t_env **current, t_env *new_node)
+{
+	if (!*head)
+	{
+		*head = new_node;
+		*current = new_node;
+	}
+	else
+	{
+		(*current)->next = new_node;
+		*current = new_node;
+	}
+}
+
+void	save_env(t_msh *msh, char **env)
+{
+	int		i;
+	t_env	*new_node;
+	t_env	*current;
+
+	i = 0;
+	msh->env = NULL;
+	current = NULL;
+	while (env[i])
+	{
+		new_node = create_env_node(env[i]);
+		if (new_node)
+			add_env_node(&msh->env, &current, new_node);
+		i++;
+	}
+}
+
+int	bi_env(t_msh *msh, char **av)
+{
+	t_env	*tmp;
+
+	if (av[1] != NULL)
+	{
+		g_exit_code = 127;
+		ft_fprintf(2, "minishell: env: %s: No such file or directory\n", av[1]);
+		return (g_exit_code);
+	}
+	tmp = msh->env;
+	while (tmp)
+	{
+		if (tmp->key && tmp->value)
+			printf("%s=%s\n", tmp->key, tmp->value);
+		tmp = tmp->next;
+	}
 	return (0);
 }
