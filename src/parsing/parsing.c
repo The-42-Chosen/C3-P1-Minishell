057e2a32f29e1c88dd2529df05fe002c1db90e5e
RicoWD <pascual.machio.eric@gmail.com>
2025-09-19 15:39:22 +0200

    (Fix): Conflits resolved

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 11:53:38 by gpollast          #+#    #+#             */
-/*   Updated: 2025/09/11 11:57:52 by gpollast         ###   ########.fr       */
+/*   Updated: 2025/09/13 19:58:52 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ char	*seek_group_redir(t_stack *stack)
 	return (NULL);
 }
 
-char	**seek_group_cmd(t_stack *stack)
+char	**seek_group_cmd(t_stack **stack)
 {
 	int		count;
 	char	**group;
@@ -60,8 +60,8 @@ char	**seek_group_cmd(t_stack *stack)
 	int		i;
 
 	count = 0;
-	tmp = stack;
-	while (tmp && tmp->token == WORD) // Vérifier tmp != NULL
+	tmp = *stack;
+	while (tmp && tmp->token == WORD)
 	{
 		count++;
 		tmp = tmp->next;
@@ -73,13 +73,12 @@ char	**seek_group_cmd(t_stack *stack)
 		return (NULL);
 	}
 	i = 0;
-	tmp = stack;
-	while (i < count && tmp) // Vérifier tmp != NULL
+	tmp = *stack;
+	while (i < count && tmp)
 	{
 		group[i] = ft_strdup(tmp->content);
-		if (!group[i]) // Vérifier ft_strdup
+		if (!group[i])
 		{
-			// Libérer la mémoire déjà allouée
 			while (--i >= 0)
 				free(group[i]);
 			free(group);
@@ -90,12 +89,12 @@ char	**seek_group_cmd(t_stack *stack)
 		tmp = tmp->next;
 	}
 	i = 0;
-	while (i < (count - 1))
+	while (i < (count - 1)) // FUITES MEMOIRES
 	{
 		i++;
-		stack = stack->next;
+		*stack = (*stack)->next;
 	}
-	group[count] = NULL; // Terminer le tableau
+	group[count] = NULL;
 	return (group);
 }
 
@@ -143,45 +142,60 @@ t_group	identify_redir(char *word)
 int	parse(t_msh *msh)
 {
 	t_stack	*tmp;
+	t_data	*current_data;
+	t_data	*new_node;
 
 	tmp = msh->stack;
+	current_data = NULL;
 	while (tmp)
 	{
 		if (err_next_token(tmp, REDIR) || err_next_token(tmp, OPERATOR))
 			return (0);
 		if (tmp->token == REDIR)
 		{
-			msh->data = init_data_node();
+			new_node = init_data_node();
+			if (!new_node)
+				return (0);
+			new_node->file_or_limiter = seek_group_redir(tmp);
+			if (!new_node->file_or_limiter)
+				return (0);
+			new_node->group = identify_redir(tmp->content);
+			ft_fprintf(1, "Expandable %d | REDIR %s\n", tmp->is_expandable, new_node->file_or_limiter);
 			if (!msh->data)
-				return (0);
-			msh->data->file_or_limiter = seek_group_redir(tmp);
-			if (!msh->data->file_or_limiter)
-				return (0);
-			msh->data->group = identify_redir(tmp->content);
-			ft_fprintf(1, "Expandable %d | REDIR %s\n", tmp->is_expandable, msh->data->file_or_limiter);
-			msh->data = msh->data->next;
+				msh->data = new_node;
+			else
+				current_data->next = new_node;
+			current_data = new_node;
 			tmp = tmp->next;
 		}
 		else if (tmp->token == WORD)
 		{
-			msh->data = init_data_node();
+			new_node = init_data_node();
+			if (!new_node)
+				return (0);
+			new_node->cmd = seek_group_cmd(&tmp);
+			if (!new_node->cmd)
+				return (0);
+			new_node->group = G_CMD;
+			print_array(new_node->cmd, "WORD");
 			if (!msh->data)
-				return (0);
-			msh->data->cmd = seek_group_cmd(tmp);
-			if (!msh->data->cmd)
-				return (0);
-			msh->data->group = G_CMD;
-			print_array(msh->data->cmd, "WORD");
-			msh->data = msh->data->next;
+				msh->data = new_node;
+			else
+				current_data->next = new_node;
+			current_data = new_node;
 		}
 		else if (tmp->token == PIPE)
 		{
-			msh->data = init_data_node();
-			if (!msh->data)
+			new_node = init_data_node();
+			if (!new_node)
 				return (0);
-			msh->data->group = G_PIPE;
+			new_node->group = G_PIPE;
 			ft_fprintf(1, "PIPE |\n");
-			msh->data = msh->data->next;
+			if (!msh->data)
+				msh->data = new_node;
+			else
+				current_data->next = new_node;
+			current_data = new_node;
 		}
 		tmp = tmp->next;
 	}
