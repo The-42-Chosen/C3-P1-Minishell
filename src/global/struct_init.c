51180f9fa17f0e25002cfdc9b37a1efd8c746890
RicoWD <pascual.machio.eric@gmail.com>
2025-09-25 23:43:20 +0200

    (fix): Builtin - Set good ordre to builtin to apply the good function when called

--- src/global/struct_init.c
+++ src/global/struct_init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   struct_init.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ep <ep@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 20:02:25 by erpascua          #+#    #+#             */
-/*   Updated: 2025/09/20 17:34:30 by gpollast         ###   ########.fr       */
+/*   Updated: 2025/09/25 23:37:29 by ep               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@ void	link_builtins(t_msh *msh)
 	msh->builtin_funcs[BI_ECHO] = &bi_echo;
 	msh->builtin_names[BI_CD] = ft_strdup("cd");
 	msh->builtin_funcs[BI_CD] = &bi_cd;
-	msh->builtin_names[BI_ENV] = ft_strdup("env");
-	msh->builtin_funcs[BI_ENV] = &bi_env;
 	msh->builtin_names[BI_PWD] = ft_strdup("pwd");
 	msh->builtin_funcs[BI_PWD] = &bi_pwd;
-	msh->builtin_names[BI_EXIT] = ft_strdup("exit");
-	msh->builtin_funcs[BI_EXIT] = &bi_exit;
 	msh->builtin_names[BI_EXPORT] = ft_strdup("export");
 	msh->builtin_funcs[BI_EXPORT] = &bi_export;
 	msh->builtin_names[BI_UNSET] = ft_strdup("unset");
 	msh->builtin_funcs[BI_UNSET] = &bi_unset;
+	msh->builtin_names[BI_ENV] = ft_strdup("env");
+	msh->builtin_funcs[BI_ENV] = &bi_env;
+	msh->builtin_names[BI_EXIT] = ft_strdup("exit");
+	msh->builtin_funcs[BI_EXIT] = &bi_exit;
 }
 
 int	struct_init(t_msh *msh)
