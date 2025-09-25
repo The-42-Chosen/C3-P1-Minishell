f543b40f3c2f8ecd4eb17a3e2279180679971152
Guillaume <guillox01@gmail.com>
2025-09-25 22:45:48 +0200

    (fix) Affichage de ^C

--- src/signals/sig_int.c
+++ src/signals/sig_int.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sig_int.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ep <ep@student.42.fr>                      +#+  +:+       +#+        */
+/*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 15:21:44 by erpascua          #+#    #+#             */
-/*   Updated: 2025/09/25 22:41:01 by ep               ###   ########.fr       */
+/*   Updated: 2025/09/25 22:44:53 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	sigint_handler(int signum)
 	g_received_signal = signum;
 	write(STDOUT_FILENO, "\n", 1);
 	rl_on_new_line();
-	rl_replace_line("", 0);
+	rl_replace_line("^C", 0);
 	rl_redisplay();
 }
 
